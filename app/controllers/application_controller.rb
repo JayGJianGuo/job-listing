dc1b8cc74ccc05d6e399bc85572b3c0de7b72ddd
jay.g <jkland927@hotmail.com>
2017-01-16 06:41:15 +0800

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_is_admin
+    if !current_user.admin?
+      flash[:alert] = "你没有管理员权限 *_*"
+      redirect_to root_path
+    else
+      flash[:notice] = "你好，管理员 ^_^"
+    end
+  end
+  
 end
